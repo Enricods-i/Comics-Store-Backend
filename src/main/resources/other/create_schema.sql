dff673843713ebe9fce02502390f243e563a55f7
Enricods <e.desalve@outlook.com>
2022-05-12 18:37:54 +0200

    Da ultimare la progettazione fisica del db

--- src/main/resources/other/create_schema.sql
+++ src/main/resources/other/create_schema.sql
@@ -1,100 +1,83 @@
-CREATE TABLE customer (
-	id BIGSERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE personal_data (
+	id BIGSERIAL PRIMARY KEY,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
-    birth_date
-    shopping_cart_id BIGINT,
+    birth_date DATE NOT NULL,
 	email VARCHAR(90) NOT NULL UNIQUE,
-	phone_number VARCHAR(20) UNIQUE,
+	phone_number VARCHAR(20),
 	address VARCHAR(50),
 	city VARCHAR(30),
 	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    modified_at TIMESTAMP NOT NULL,
-	admin BOOLEAN NOT NULL DEFAULT False,
-    CONSTRAINT fk_customer_cart
-    FOREIGN KEY (shopping_cart_id)
-    REFERENCES shopping_cart(id)
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE collection (
-    name VARCHAR(50) NOT NULL PRIMARY KEY,
-    image VARCHAR(100)
+    name VARCHAR(50) PRIMARY KEY,
+    image VARCHAR(100),
+    description VARCHAR(1000)
+);
+
+CREATE TABLE cart_data (
+    user BIGINT PRIMARY KEY REFERENCES personal_data (id),
+    total DECIMAL(7,2) NOT NULL,
+    size INT,
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE comic (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    collection_id BIGINT,
+    id BIGSERIAL PRIMARY KEY,
+    collection BIGINT REFERENCES collection (name),
 	number INT,
 	price DECIMAL(5,2) NOT NULL,
 	quantity INT NOT NULL,
 	image VARCHAR(100),
 	writers VARCHAR(50),
-	draftsmans VARCHAR(50),
+	catoonists VARCHAR(50),
+    format_and_binding 
 	pages INT,
     format_binding VARCHAR(50)
-	isbn VARCHAR(50) NOT NULL UNIQUE,
-	description TEXT,
-    CONSTRAINT fk_comic_collection
-    FOREIGN KEY (collection_id)
-    REFERENCES collection(id)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE
+	isbn VARCHAR(13) NOT NULL UNIQUE,
+	description VARCHAR(200),
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE shopping_cart (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    total DECIMAL(7,2) NOT NULL,
-    size INT,
-    modified_at TIMESTAMP NOT NULL
+CREATE TABLE cart_content(
+    cart BIGINT REFERENCES cart_data (user),
+    comic BIGINT REFERENCES comic (id),
+    quantity INT
+    PRIMARY KEY (cart,comic)
 );
 
-CREATE TABLE cart_content (
-    
+CREATE TABLE wish_list(
+    id BIGSERIAL PRIMARY KEY,
+    user BIGINT REFERENCES personal_data (id),
+    name VARCHAR(70) NOT NULL,
+    notifications BOOLEAN DEFAULT FALSE,
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    modified_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE cart_item (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    shopping_cart_id BIGINT NOT NULL,
-    comic_id BIGINT NOT NULL,
-    quantity INT NOT NULL DEFAULT 1,
-    CONSTRAINT fk_cartitem_shoppingcart
-    FOREIGN KEY (shopping_cart_id)
-    REFERENCES shopping_cart(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT fk_cartitem_comic
-    FOREIGN KEY (comic_id)
-    REFERENCES comic(id)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE
+CREATE TABLE list_content(
+    list BIGINT REFERENCES wish_list (id),
+    comic BIGINT REFERENCES comic (id),
+    PRIMARY KEY (list,comic)
 );
 
-CREATE TABLE order_details (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    customer_id BIGINT NOT NULL,
-    total DECIMAL(7,2) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    modified_at TIMESTAMP,
-    CONSTRAINT fk_orderdetails_customer
-    FOREIGN KEY (customer_id)
-    REFERENCES customer(id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+CREATE TABLE category(
+    name VARCHAR(50) PRIMARY KEY,
+    description VARCHAR(200)
 );
 
-CREATE TABLE order_item (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    order_details_id BIGINT NOT NULL,
-    comic_id BIGINT NOT NULL,
-    quantity INT NOT NULL DEFAULT 1,
-    CONSTRAINT fk_orderitem_orderdetails
-    FOREIGN KEY (order_details_id)
-    REFERENCES order_details(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-    CONSTRAINT fk_orderitem_comic
-    FOREIGN KEY (comic_id)
-    REFERENCES comic(id)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+CREATE TABLE classification(
+    category BIGINT REFERENCES category (name),
+    collection BIGINT REFERENCES collection (name),
+    PRIMARY KEY (category,collection)
+);
+
+CREATE TABLE discount(
+    id BIGSERIAL PRIMARY KEY,
+    percentage
+    expiration_date
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
