017f99033a7d6432db25f03cba662f71011e605b
Enricods <e.desalve@outlook.com>
2022-07-27 09:26:33 +0200

    Modificato il pom e l'application.yaml per impostare la sicurezza: autenticazione e https

---
+++ src/main/java/im/enricods/ComicsStore/configurations/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package im.enricods.ComicsStore.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+    }//configure
+
+
+    @Bean
+    CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        //configuration.setAllowCredentials(true);
+        configuration.applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }//corsFilter
+
+}//SecurityConfiguration
