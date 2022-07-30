27973911d65208e4adbaf318f0c3a67a89862140
Enricods-i <e.desalve@outlook.com>
2022-07-30 19:29:02 +0200

    DB: Finiti gli import

---
+++ src/main/resources/other/imports/7_category.sql
@@ -0,0 +1,8 @@
+insert into category (id, name) values (1, 'Manga');
+insert into category (id, name) values (2, 'Fumetto Italiano');
+insert into category (id, name) values (3, 'Fumetto Americano');
+insert into category (id, name) values (4, 'Azione');
+insert into category (id, name) values (5, 'Avventura');
+insert into category (id, name) values (6, 'Horror');
+insert into category (id, name) values (7, 'Umoristico');
+insert into category (id, name) values (8, 'Supereroi');
