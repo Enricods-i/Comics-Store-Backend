bf36c597ed3e6afcb08ee0c6187178701e8b298f
Enricods-i <e.desalve@outlook.com>
2022-08-02 17:04:51 +0200

    Aggiunta la classe Image per salvare e recuperare le immagini di Fumetti, Collezioni ed Autori Aggiunta una dipendenza nel pom.xml che aggiunge un convertitore di immagini nel formato webp

---
+++ src/main/java/im/enricods/ComicsStore/utils/Image.java
@@ -0,0 +1,30 @@
+package im.enricods.ComicsStore.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+public class Image {
+
+    @Value("$(image-path)")
+    private static String IMAGES_DIRECTORY;
+
+    public static void saveImage(String fileName, MultipartFile file) throws IOException{
+        BufferedImage image = ImageIO.read(file.getInputStream());
+        if(image.getWidth() != 435 || image.getHeight() != 682)
+            throw new IllegalArgumentException("Image not valid! It must be 435x682");
+
+        //save image to the server
+        ImageIO.write(image, "webp", new File(IMAGES_DIRECTORY + fileName + ".webp"));
+    }//saveImage
+
+    public static java.awt.Image getImage(String fileName) throws IOException{
+        return ImageIO.read(new File(IMAGES_DIRECTORY+fileName+".webp"));
+    }//getImage
+    
+}//Image
