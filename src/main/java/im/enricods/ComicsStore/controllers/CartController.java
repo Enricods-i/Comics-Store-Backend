254553a0906b3d07d35d99848d845dada656103d
Enricods-i <e.desalve@outlook.com>
2022-08-16 15:13:13 +0200

    SE-CO: Cart sistemato

--- src/main/java/im/enricods/ComicsStore/controllers/CartController.java
+++ src/main/java/im/enricods/ComicsStore/controllers/CartController.java
@@ -14,12 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import im.enricods.ComicsStore.entities.Cart;
 import im.enricods.ComicsStore.services.CartService;
 import im.enricods.ComicsStore.utils.InvalidValue;
-import im.enricods.ComicsStore.utils.exceptions.CartNotFoundException;
-import im.enricods.ComicsStore.utils.exceptions.ComicAlreadyExistsException;
-import im.enricods.ComicsStore.utils.exceptions.ComicNotFoundException;
-import im.enricods.ComicsStore.utils.exceptions.ComicNotInCartException;
-import im.enricods.ComicsStore.utils.exceptions.ComicsQuantityUnavaiableException;
-import im.enricods.ComicsStore.utils.exceptions.UserNotFoundException;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,42 +29,30 @@ public class CartController {
     private CartService cartService;
 
     @GetMapping
-    public ResponseEntity<?> getCart(@RequestParam(value = "usr") long userId) {
+    public ResponseEntity<?> get(@RequestParam(value = "usr") long userId) {
         try{
-            Cart result = cartService.getCartByUser(userId);
+            Cart result = cartService.getByUser(userId);
             return new ResponseEntity<Cart>(result, HttpStatus.OK);
         }
         catch(ConstraintViolationException e){
             return new ResponseEntity<List<InvalidValue>>(InvalidValue.getAllInvalidValues(e), HttpStatus.BAD_REQUEST);
         }
-        catch(UserNotFoundException e){
-            return new ResponseEntity<String>("User \""+userId+"\" not found",HttpStatus.BAD_REQUEST);
-        }
-        catch(CartNotFoundException e){
-            return new ResponseEntity<String>("Cart for user \""+userId+"\" not found",HttpStatus.BAD_REQUEST);
+        catch(IllegalArgumentException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }//getCart
     
     @PostMapping
     public ResponseEntity<?> addComic(@RequestParam(value = "usr") long userId, @RequestParam(value = "cmcId") long comicId, @RequestParam(value = "qty") int quantity){
         try{
-            cartService.addComicToUsersCart(userId, comicId, quantity);
+            cartService.addComic(userId, comicId, quantity);
             return new ResponseEntity<String>("Comic with id \""+ comicId +"\" added succesul!", HttpStatus.OK);
         }
         catch(ConstraintViolationException e){
             return new ResponseEntity<List<InvalidValue>>(InvalidValue.getAllInvalidValues(e), HttpStatus.BAD_REQUEST);
         }
-        catch(UserNotFoundException e){
-            return new ResponseEntity<String>("User \""+userId+"\" not found", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicNotFoundException e){
-            return new ResponseEntity<String>("Comic with id \"" + comicId + "\" not found!", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicAlreadyExistsException e){
-            return new ResponseEntity<String>("Comic "+comicId+" is already in Cart", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicsQuantityUnavaiableException e){
-            return new ResponseEntity<String>("Quantity unavaiable!", HttpStatus.BAD_REQUEST);
+        catch(IllegalArgumentException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }//addComic
 
@@ -83,55 +65,23 @@ public class CartController {
         catch(ConstraintViolationException e){
             return new ResponseEntity<List<InvalidValue>>(InvalidValue.getAllInvalidValues(e), HttpStatus.BAD_REQUEST);
         }
-        catch(UserNotFoundException e){
-            return new ResponseEntity<String>("User \""+userId+"\" not found", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicNotFoundException e){
-            return new ResponseEntity<String>("Comic with id \"" + comicId + "\" not found!", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicNotInCartException e){
-            return new ResponseEntity<String>("Comic with id \"" + comicId + "\" not found in your cart!", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicsQuantityUnavaiableException e){
-            return new ResponseEntity<String>("Quantity unavaiable!", HttpStatus.BAD_REQUEST);
+        catch(IllegalArgumentException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }//updateComicQuantity
 
     @DeleteMapping
     public ResponseEntity<?> deleteComic(@RequestParam(value = "usr") long userId, @RequestParam(value = "cmcId") long comicId){
         try{
-            cartService.deleteComicFromUsersCart(userId, comicId);
+            cartService.removeComic(userId, comicId);
             return new ResponseEntity<String>("Comic deleted succesul!", HttpStatus.OK);
         }
         catch(ConstraintViolationException e){
             return new ResponseEntity<List<InvalidValue>>(InvalidValue.getAllInvalidValues(e), HttpStatus.BAD_REQUEST);
         }
-        catch(UserNotFoundException e){
-            return new ResponseEntity<String>("User \""+userId+"\" not found", HttpStatus.BAD_REQUEST);
+        catch(IllegalArgumentException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-        catch(ComicNotFoundException e){
-            return new ResponseEntity<String>("Comic with id \"" + comicId + "\" not found!", HttpStatus.BAD_REQUEST);
-        }
-        catch(ComicNotInCartException e){
-            return new ResponseEntity<String>("Comic with id \"" + comicId + "\" not found in your cart!", HttpStatus.BAD_REQUEST);
-        }
-    }//addComic
-
-    @PutMapping(path = "/readReceipts")
-    public ResponseEntity<?> readReceipts(@RequestParam(value = "usr") long userId){
-        try {
-            cartService.readReceipt(userId);
-            return new ResponseEntity<String>(HttpStatus.OK);
-        }
-        catch(ConstraintViolationException e){
-            return new ResponseEntity<List<InvalidValue>>(InvalidValue.getAllInvalidValues(e), HttpStatus.BAD_REQUEST);
-        }
-        catch(UserNotFoundException e){
-            return new ResponseEntity<String>("User \""+userId+"\" not found",HttpStatus.BAD_REQUEST);
-        }
-        catch(CartNotFoundException e){
-            return new ResponseEntity<String>("Cart for user \""+userId+"\" not found",HttpStatus.BAD_REQUEST);
-        }
-    }//readReceipts
+    }//deleteComic
 
 }//CartController
